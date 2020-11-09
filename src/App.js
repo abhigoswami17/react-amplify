c6ec5c9f6175b2282a89ee3bccb3a12ee6b00b28
Abhishek Goswami <abhishek.gswmi11@gmail.com>
2020-11-09 23:32:18 +0530

    Changes for V2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and React Native
         </a>
       </header>
     </div>
